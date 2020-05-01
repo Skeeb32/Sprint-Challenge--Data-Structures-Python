c1b8d9c113d0a249e22da8e6a7f75b5bcd3e4af5
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-05-01 15:55:28 -0700

    DS

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -10,25 +10,38 @@ class RingBuffer:
     def append(self, item):
         if len(self.storage) < self.capacity:
             self.storage.add_to_tail(item)
+            self.current = self.storage.tail
         else:
-            if self.current == None or self.current.next == None:
+            if self.current == self.storage.tail:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
                 self.current = self.storage.head
             else:
+                self.storage.insert_after(self.current, item)
                 self.current = self.current.next
-            self.current.value = item
+                self.storage.delete(self.current.next)
+                
+    def __next_from_current(self):
+        if len(self.storage) == self.capacity:
+            if self.current.next:
+                return self.current.next
+            else:
+                return self.storage.head
+        else: 
+            return self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         node = self.storage.head
-        while node != None:
+        while node:
             list_buffer_contents.append(node.value)
             node = node.next
-            
+
         return list_buffer_contents
 
-# ----------------Stretch Goal-------------------
+        # ----------------Stretch Goal-------------------
 
 
 class ArrayRingBuffer:
